877619ac40669df56050ea92d2db11b55b677c30
Gianluca Mazzeo <gianlucamazzeo@gmail.com>
2025-05-05 11:54:39 +0200

    Implements role-based access control for greeting

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Roles } from './auth/decorators/roles.decorator';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { RolesGuard } from './auth/guards/roles.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
   getHello(): string {
     return this.appService.getHello();
   }
